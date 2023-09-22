0b5b46fcd5fb2be46f230c02177baf608a1bacf5
Bruna Mirelly Ferreira Paixão <20211170150070@ifrn.local>
2023-09-22 14:23:27 -0300

    Ajuste de Camera

--- Jogo3D/Assets/Script/Player.cs
+++ Jogo3D/Assets/Script/Player.cs
@@ -5,34 +5,62 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
-
     private CharacterController controller;
+    private Transform cam;
+    private Vector3 moveDirection;
+    public float gravity;
+
 
     public float smoothRotTime;
     private float turnSmoothVelocity;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        cam = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
-
-
-        if (direction.magnitude > 0)
-        {
-            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
-            transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
-            controller.Move(direction * speed * Time.deltaTime);
-            
-        }
+        Move();
     }
+
+
+    private void Move()
+    {
+        if (controller.isGrounded)
+        {
+            //pegar a entrada na horizontal (Tecla direita/esquerda)
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            //pegar a entrada na horizontal (Tecla Cima/Baixo)
+            float vertical = Input.GetAxisRaw("Vertical");
+            // Variavel local que armazena o valor de eixo horizontal e vertical
+            Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+            //verifica se o personagem esta se movimentando (se for > 0) 
+            if (direction.magnitude > 0)
+            {
+                //armazena a rotação e o angulo da camera
+                float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+                //aemazena a rotação mais suave
+                float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity,
+                    smoothRotTime);
+
+                //rotaciona o personagem
+                transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+
+                //armazena a direção
+                moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
+
+            }
+
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * speed * Time.deltaTime);
+    }
+
 }
