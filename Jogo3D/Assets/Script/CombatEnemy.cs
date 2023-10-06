723497d6b7e06f7da1528df6bad12ed1e869b7a0
Bruna Mirelly Ferreira Paixão <20211170150070@ifrn.local>
2023-10-06 16:22:21 -0300

    Programando Animações do Inimigo

--- Jogo3D/Assets/Script/CombatEnemy.cs
+++ Jogo3D/Assets/Script/CombatEnemy.cs
@@ -7,21 +7,27 @@ using UnityEngine.AI;
 public class CombatEnemy : MonoBehaviour
 {
     [Header("Atributes")]
-    public float totalDamage;
+    public float totalHealth;
     public float attackDamage;
     public float movementSpeed;
     public float lookRadius;
+    public float colliderRadius = 2;
 
     [Header("Components")]
-    private Animation anim;
+    private Animator anim;
     private CapsuleCollider capsule;
     private NavMeshAgent Agent;
 
     [Header("Others")] 
     private Transform player;
+
+    private bool walking;
+    private bool attacking;
+    private bool Hiting;
+    private bool waitfor;
     void Start()
     {
-        anim = GetComponent<Animation>();
+        anim = GetComponent<Animator>();
         capsule = GetComponent<CapsuleCollider>();
         Agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -33,18 +39,85 @@ public class CombatEnemy : MonoBehaviour
         float distance = Vector3.Distance(player.position, transform.position);
         if ((distance <= lookRadius))
         {
-            Agent.SetDestination(player.position);
             //O personagem esta no raio de ação
+            Agent.isStopped = false;
+            if (!attacking)
+            {
+                Agent.SetDestination(player.position);
+                anim.SetBool("Walk Forward", true);
+                walking = true;
+            }
             if (distance <= Agent.stoppingDistance)
             {
-                Debug.Log("ATACAR");
+                StartCoroutine("Attack");
+            }
+            else
+            {
+                attacking = false;
             }
         }
         else
         {
             //O personagem não esta no raio de ação
-            
+            anim.SetBool("Walk Forward", false);
+            Agent.isStopped = true;
+            walking = false;
+            attacking = false;
+
         }
+    }
+    
+    IEnumerator Attack()
+    {
+        if (!waitfor)
+        {
+            waitfor = true;
+            attacking = true;
+            walking = false;
+            anim.SetBool("Web Attack", true);
+            anim.SetBool("Walk Forward", false);
+            yield return new WaitForSeconds(1f);
+            GetPlayer();
+            yield return new WaitForSeconds(1f);
+            waitfor = false;
+        }
+    }
+
+    void GetPlayer()
+    {
+        foreach (Collider c in Physics.OverlapSphere((transform.position + transform.forward * colliderRadius), colliderRadius))
+        {
+            if (c.gameObject.CompareTag("Player"))
+            {
+              //vai causar dano do player
+              Debug.Log("Bateu");
+            }
+        }
+    }
+
+    public void GetHit(float damage)
+    {
+        totalHealth -= damage;
+        if (totalHealth > 0)
+        {
+            //esta vivo
+            StopCoroutine("Attack");
+            anim.SetTrigger("Take Damage");
+            Hiting = true;
+        }
+        else
+        {
+            //esta morto
+        }
+    }
+
+    IEnumerator RecorveryFromHit()
+    {
+        yield return new WaitForSeconds(1f);
+        anim.SetBool("Walk Forward", false);
+        anim.SetBool("Web Attack", false);
+        Hiting = false;
+        waitfor = false;
     }
 
     private void OnDrawGizmosSelected()
